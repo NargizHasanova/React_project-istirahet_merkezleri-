a7e7a47612c0e563ea01fe4d2f1221e5b55c7bb5
NargizHasanova <nargiz.gasanova.1996@mail.ru>
2021-09-11 17:49:19 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import SanatoriumItem from './components/sanatorium/SanatoriumItem';
 
 
 function App() {
+
   return (
     <BrowserRouter>
       <Switch>
--- src/components/beach/BeachItem.jsx
+++ src/components/beach/BeachItem.jsx
@@ -6,6 +6,24 @@ import CommentItem from "./CommentItem"
 import { useContext, useState } from "react"
 import { CommentContext } from "../../Context"
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+
+
+
+// import Swiper core and required modules
+import SwiperCore, {
+    Navigation, Thumbs
+} from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([Navigation, Thumbs]);
+
+
+
 export default function CardItem() {
     const { id } = useParams()
     const filtered = beach.filter(item => item.id === parseInt(id))
@@ -17,8 +35,16 @@ export default function CardItem() {
         setComment([{ id: Date.now(), comment: addValue }, ...comment])
         setAddValue("")
     }
+
+    const [thumbsSwiper, setThumbsSwiper] = useState(null);
     return (
         <>
+            <Swiper style={{ '--swiper-navigation-color': '#fff', '--swiper-pagination-color': '#fff' }} spaceBetween={10} navigation={true} thumbs={{ swiper: thumbsSwiper }} className="mySwiper2">
+                <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-1.jpg" /></SwiperSlide><SwiperSlide><img src="https://swiperjs.com/demos/images/nature-2.jpg" /></SwiperSlide><SwiperSlide><img src="https://swiperjs.com/demos/images/nature-3.jpg" /></SwiperSlide><SwiperSlide><img src="https://swiperjs.com/demos/images/nature-4.jpg" /></SwiperSlide><SwiperSlide><img src="https://swiperjs.com/demos/images/nature-5.jpg" /></SwiperSlide><SwiperSlide><img src="https://swiperjs.com/demos/images/nature-6.jpg" /></SwiperSlide><SwiperSlide><img src="https://swiperjs.com/demos/images/nature-7.jpg" /></SwiperSlide><SwiperSlide><img src="https://swiperjs.com/demos/images/nature-8.jpg" /></SwiperSlide><SwiperSlide><img src="https://swiperjs.com/demos/images/nature-9.jpg" /></SwiperSlide><SwiperSlide><img src="https://swiperjs.com/demos/images/nature-10.jpg" /></SwiperSlide>
+            </Swiper>
+            <Swiper onSwiper={setThumbsSwiper} spaceBetween={10} slidesPerView={4} freeMode={true} watchSlidesProgress={true} className="mySwiper">
+                <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-1.jpg" /></SwiperSlide><SwiperSlide><img src="https://swiperjs.com/demos/images/nature-2.jpg" /></SwiperSlide><SwiperSlide><img src="https://swiperjs.com/demos/images/nature-3.jpg" /></SwiperSlide><SwiperSlide><img src="https://swiperjs.com/demos/images/nature-4.jpg" /></SwiperSlide><SwiperSlide><img src="https://swiperjs.com/demos/images/nature-5.jpg" /></SwiperSlide><SwiperSlide><img src="https://swiperjs.com/demos/images/nature-6.jpg" /></SwiperSlide><SwiperSlide><img src="https://swiperjs.com/demos/images/nature-7.jpg" /></SwiperSlide><SwiperSlide><img src="https://swiperjs.com/demos/images/nature-8.jpg" /></SwiperSlide><SwiperSlide><img src="https://swiperjs.com/demos/images/nature-9.jpg" /></SwiperSlide><SwiperSlide><img src="https://swiperjs.com/demos/images/nature-10.jpg" /></SwiperSlide>
+            </Swiper>
             <div onClick={() => history.goBack()} className="pageBack">
                 <i className="fas fa-chevron-left"></i>
                 <span>geri</span>
